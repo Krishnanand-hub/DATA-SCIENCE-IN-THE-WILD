ba36d5adb0a9373cda24a6bceab6524f5a7ed348
josep <josephhall1000@gmail.com>
2026-03-04 10:25:45 +0000

    Joe changes to portal, addition of geo data and requirements.txt

--- dataLoader.py
+++ dataLoader.py
@@ -36,10 +36,51 @@ def link_district_region(FORCE_PROCESSING=False):
 
     return df
 
+def read_ons_pd(FORCE_PROCESSING=False):
+    if FORCE_PROCESSING or not os.path.exists("./data/read_ons_pd.csv"):
+        df = pandas.read_excel("./data/ONSPD_NOV_2025_UK_-4615871342515732933.xlsx")
+        df.to_csv("./data/read_ons_pd.csv", index=False)
+    else: df = pandas.read_csv("./data/read_ons_pd.csv")
+    return df
+
+def hpi_2025_data(FORCE_PROCESSING=False):
+    if FORCE_PROCESSING or not os.path.exists("./data/hpi_2025_data.csv"):
+        df_englad = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="8", skiprows=2)
+        df_wales = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="9", skiprows=2)
+        df_scotland = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="10", skiprows=2)
+        df_north_ireland = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="11", skiprows=2)
+        df = pandas.concat([df_englad, df_wales, df_scotland, df_north_ireland])
+        df.to_csv("./data/hpi_2025_data.csv", index=False)
+
+    else: df = pandas.read_excel("./data/hpi_2025_data.csv")
+
+    return df
+
+def historical_price_data(FORCE_PROCESSING=False):
+    if FORCE_PROCESSING or not os.path.exists("./data/historical_price_data.csv"):
+        rename_column = pandas.read_csv("./data/region_country_code.csv")
+        rename_column = dict(zip(rename_column["Region"], rename_column["Code"]))
+        df = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="2", skiprows=2)
+        df.to_csv("./data/historical_price_data.csv", index=False)
+        df = df.rename(columns=rename_column)
+    else: df = pandas.read_excel("./data/historical_price_data.csv")
+
+    return df
+
+def historical_hpi_data(FORCE_PROCESSING=False):
+    if FORCE_PROCESSING or not os.path.exists("./data/historical_data.csv"):
+        rename_column = pandas.read_csv("./data/region_country_code.csv")
+        rename_column = dict(zip(rename_column["Region"], rename_column["Code"]))
+
+        df = pandas.read_excel("./data/ukhousepriceindexmonthlypricestatistics.xlsx", sheet_name="3", skiprows=2)
+        df = df.rename(columns=rename_column)
+        df.to_csv("./data/historical_data.csv", index=False)
+
+
+    else: df = pandas.read_excel("./data/historical_data.csv")
+
+    return df
+
 if __name__ == "__main__":
-    data = link_postcode_district()
-    print(data.head())
-    data = link_postcode_region()
-    print(data.head())
-    data = link_district_region()
+    data = historical_price_data(FORCE_PROCESSING=True)
     print(data.head())
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,12 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
-portal_blueprint = Blueprint('portal', __name__, template_folder='templates')
+from dataLoader import read_ons_pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas
+import plotly.express as px
+
+portal_blueprint = Blueprint('portal', __name__, template_folder='templates', static_folder='static')
 
 @portal_blueprint.route("/portal")
 def portal():
-    return render_template("main/construction.html")
+    return render_template("main/portal.html")  # EXPECTS MAP IN FIGURES
